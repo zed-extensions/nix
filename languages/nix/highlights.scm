9c3b3f36933f5d9680ad62b76cdf26b1b8a2e198
Mathias <hi@mathias.ninja>
2024-11-17 22:17:36 +0100

    fix: use more up to date `injections.scm` & `highlights.scm` (#16)
    Used more up to date `injections.scm` & `highlights.scm` from
    <https://github.com/nvim-treesitter/nvim-treesitter/tree/master/queries/nix>

--- languages/nix/highlights.scm
+++ languages/nix/highlights.scm
@@ -1,74 +1,56 @@
-(comment) @comment
+; basic keywords
+[
+  "assert"
+  "in"
+  "inherit"
+  "let"
+  "rec"
+  "with"
+] @keyword
 
+; if/then/else
 [
   "if"
   "then"
   "else"
-  "let"
-  "inherit"
-  "in"
-  "rec"
-  "with"
-  "assert"
-  "or"
-] @keyword
+] @keyword.conditional
 
-((identifier) @variable.builtin
- (#match? @variable.builtin "^(__currentSystem|__currentTime|__nixPath|__nixVersion|__storeDir|builtins|false|null|true)$")
- (#is-not? local))
+; field access default (`a.b or c`)
+"or" @keyword.operator
 
-((identifier) @function.builtin
- (#match? @function.builtin "^(__add|__addErrorContext|__all|__any|__appendContext|__attrNames|__attrValues|__bitAnd|__bitOr|__bitXor|__catAttrs|__compareVersions|__concatLists|__concatMap|__concatStringsSep|__deepSeq|__div|__elem|__elemAt|__fetchurl|__filter|__filterSource|__findFile|__foldl'|__fromJSON|__functionArgs|__genList|__genericClosure|__getAttr|__getContext|__getEnv|__hasAttr|__hasContext|__hashFile|__hashString|__head|__intersectAttrs|__isAttrs|__isBool|__isFloat|__isFunction|__isInt|__isList|__isPath|__isString|__langVersion|__length|__lessThan|__listToAttrs|__mapAttrs|__match|__mul|__parseDrvName|__partition|__path|__pathExists|__readDir|__readFile|__replaceStrings|__seq|__sort|__split|__splitVersion|__storePath|__stringLength|__sub|__substring|__tail|__toFile|__toJSON|__toPath|__toXML|__trace|__tryEval|__typeOf|__unsafeDiscardOutputDependency|__unsafeDiscardStringContext|__unsafeGetAttrPos|__valueSize|abort|baseNameOf|derivation|derivationStrict|dirOf|fetchGit|fetchMercurial|fetchTarball|fromTOML|import|isNull|map|placeholder|removeAttrs|scopedImport|throw|toString)$")
- (#is-not? local))
+; comments
+(comment) @comment ; @spell
 
+; strings
+(string_fragment) @string
+
+(string_expression
+  "\"" @string)
+
+(indented_string_expression
+  "''" @string)
+
+; paths and URLs
 [
-  (integer_expression)
-  (float_expression)
-] @number
+  (path_expression)
+  (hpath_expression)
+  (spath_expression)
+] @string.special.path
 
-(escape_sequence) @escape
-(dollar_escape) @escape
+(uri_expression) @string.special.url
 
-(function_expression
-  universal: (identifier) @variable.parameter
-)
+; escape sequences
+(escape_sequence) @string.escape
 
-(formal
-  name: (identifier) @variable.parameter
-  "?"? @punctuation.delimiter)
-
-(select_expression
-  attrpath: (attrpath (identifier)) @property)
-
-(apply_expression
-  function: [
-    (variable_expression (identifier)) @function
-    (select_expression
-      attrpath: (attrpath
-        attr: (identifier) @function .))])
-
-(unary_expression
-  operator: _ @operator)
-
-(binary_expression
-  operator: _ @operator)
-
-(variable_expression (identifier) @variable)
-
-(binding
-  attrpath: (attrpath (identifier)) @property)
-
-(identifier) @property
-
-(inherit_from attrs: (inherited_attrs attr: (identifier) @property) )
-
+; delimiters
 [
-  ";"
   "."
+  ";"
+  ":"
   ","
-  "="
 ] @punctuation.delimiter
 
+; brackets
 [
   "("
   ")"
@@ -78,22 +60,147 @@
   "}"
 ] @punctuation.bracket
 
-(identifier) @variable
+; `?` in `{ x ? y }:`, used to set defaults for named function arguments
+(formal
+  name: (identifier) @variable.parameter
+  "?"? @operator)
 
-[
-  (string_expression)
-  (indented_string_expression)
-] @string
+; `...` in `{ ... }`, used to ignore unknown named function arguments (see above)
+(ellipses) @variable.parameter.builtin
 
-[
-  (path_expression)
-  (hpath_expression)
-  (spath_expression)
-] @string.special.path
+; universal is the parameter of the function expression
+; `:` in `x: y`, used to separate function argument from body (see above)
+(function_expression
+  universal: (identifier) @variable.parameter
+  ":" @punctuation.special)
 
-(uri_expression) @string.special.uri
+; function calls
+(apply_expression
+  function: (variable_expression
+    name: (identifier) @function.call))
 
+; basic identifiers
+(variable_expression) @variable
+
+(variable_expression
+  name: (identifier) @keyword.import
+  (#eq? @keyword.import "import"))
+
+(variable_expression
+  name: (identifier) @boolean
+  (#any-of? @boolean "true" "false"))
+
+; builtin functions (with builtins prefix)
+(select_expression
+  expression: (variable_expression
+    name: (identifier) @_id)
+  attrpath: (attrpath
+    attr: (identifier) @function.builtin)
+  (#eq? @_id "builtins"))
+
+; builtin functions (without builtins prefix)
+(variable_expression
+  name: (identifier) @function.builtin
+  (#any-of? @function.builtin
+    ; nix eval --impure --expr 'with builtins; filter (x: !(elem x [ "abort" "import" "throw" ]) && isFunction builtins.${x}) (attrNames builtins)'
+    "add" "addErrorContext" "all" "any" "appendContext" "attrNames" "attrValues" "baseNameOf"
+    "bitAnd" "bitOr" "bitXor" "break" "catAttrs" "ceil" "compareVersions" "concatLists" "concatMap"
+    "concatStringsSep" "deepSeq" "derivation" "derivationStrict" "dirOf" "div" "elem" "elemAt"
+    "fetchGit" "fetchMercurial" "fetchTarball" "fetchTree" "fetchurl" "filter" "filterSource"
+    "findFile" "floor" "foldl'" "fromJSON" "fromTOML" "functionArgs" "genList" "genericClosure"
+    "getAttr" "getContext" "getEnv" "getFlake" "groupBy" "hasAttr" "hasContext" "hashFile"
+    "hashString" "head" "intersectAttrs" "isAttrs" "isBool" "isFloat" "isFunction" "isInt" "isList"
+    "isNull" "isPath" "isString" "length" "lessThan" "listToAttrs" "map" "mapAttrs" "match" "mul"
+    "parseDrvName" "partition" "path" "pathExists" "placeholder" "readDir" "readFile" "removeAttrs"
+    "replaceStrings" "scopedImport" "seq" "sort" "split" "splitVersion" "storePath" "stringLength"
+    "sub" "substring" "tail" "toFile" "toJSON" "toPath" "toString" "toXML" "trace" "traceVerbose"
+    "tryEval" "typeOf" "unsafeDiscardOutputDependency" "unsafeDiscardStringContext"
+    "unsafeGetAttrPos" "zipAttrsWith"
+    ; primops, `__<tab>` in `nix repl`
+    "__add" "__filter" "__isFunction" "__split" "__addErrorContext" "__filterSource" "__isInt"
+    "__splitVersion" "__all" "__findFile" "__isList" "__storeDir" "__any" "__floor" "__isPath"
+    "__storePath" "__appendContext" "__foldl'" "__isString" "__stringLength" "__attrNames"
+    "__fromJSON" "__langVersion" "__sub" "__attrValues" "__functionArgs" "__length" "__substring"
+    "__bitAnd" "__genList" "__lessThan" "__tail" "__bitOr" "__genericClosure" "__listToAttrs"
+    "__toFile" "__bitXor" "__getAttr" "__mapAttrs" "__toJSON" "__catAttrs" "__getContext" "__match"
+    "__toPath" "__ceil" "__getEnv" "__mul" "__toXML" "__compareVersions" "__getFlake" "__nixPath"
+    "__trace" "__concatLists" "__groupBy" "__nixVersion" "__traceVerbose" "__concatMap" "__hasAttr"
+    "__parseDrvName" "__tryEval" "__concatStringsSep" "__hasContext" "__partition" "__typeOf"
+    "__currentSystem" "__hashFile" "__path" "__unsafeDiscardOutputDependency" "__currentTime"
+    "__hashString" "__pathExists" "__unsafeDiscardStringContext" "__deepSeq" "__head" "__readDir"
+    "__unsafeGetAttrPos" "__div" "__intersectAttrs" "__readFile" "__zipAttrsWith" "__elem"
+    "__isAttrs" "__replaceStrings" "__elemAt" "__isBool" "__seq" "__fetchurl" "__isFloat" "__sort"))
+
+; constants
+(variable_expression
+  name: (identifier) @constant.builtin
+  (#any-of? @constant.builtin
+    ; nix eval --impure --expr 'with builtins; filter (x: !(isFunction builtins.${x} || isBool builtins.${x})) (attrNames builtins)'
+    "builtins" "currentSystem" "currentTime" "langVersion" "nixPath" "nixVersion" "null" "storeDir"))
+
+; string interpolation (this was very annoying to get working properly)
 (interpolation
   "${" @punctuation.special
-  (_) @embedded
-  "}" @punctuation.special)
+  (_)
+  "}" @punctuation.special) @none
+
+(select_expression
+  expression: (_) @_expr
+  attrpath: (attrpath
+    attr: (identifier) @variable.member)
+  (#not-eq? @_expr "builtins"))
+
+(attrset_expression
+  (binding_set
+    (binding
+      .
+      (attrpath
+        (identifier) @variable.member))))
+
+(rec_attrset_expression
+  (binding_set
+    (binding
+      .
+      (attrpath
+        (identifier) @variable.member))))
+
+; function definition
+(binding
+  attrpath: (attrpath
+    attr: (identifier) @function)
+  expression: (function_expression))
+
+; unary operators
+(unary_expression
+  operator: _ @operator)
+
+; binary operators
+(binary_expression
+  operator: _ @operator)
+
+[
+  "="
+  "@"
+  "?"
+] @operator
+
+; integers, also highlight a unary -
+[
+  (unary_expression
+    "-"
+    (integer_expression))
+  (integer_expression)
+] @number
+
+; floats, also highlight a unary -
+[
+  (unary_expression
+    "-"
+    (float_expression))
+  (float_expression)
+] @number.float
+
+; exceptions
+(variable_expression
+  name: (identifier) @keyword.exception
+  (#any-of? @keyword.exception "abort" "throw"))
--- languages/nix/injections.scm
+++ languages/nix/injections.scm
@@ -1,39 +1,190 @@
-; mark arbitary languages with a comment
-((((comment) @injection.language) .
-  (indented_string_expression (string_fragment) @injection.content))
-  (#set! injection.combined))
+; ((comment) @content
+;   (#set! language "comment"))
 
-((binding
-   attrpath: (attrpath (identifier) @_path)
-   expression: (indented_string_expression
-     (string_fragment) @injection.content))
- (#match? @_path "(^\\w*Phase|(pre|post)\\w*|(.*\\.)?\\w*([sS]cript|[hH]ook)|(.*\\.)?startup)$")
- (#set! injection.language "bash")
- (#set! injection.combined))
+; ((comment) @language
+;   . ; this is to make sure only adjacent comments are accounted for the injections
+;   [
+;     (string_expression
+;       (string_fragment) @content)
+;     (indented_string_expression
+;       (string_fragment) @content)
+;   ]
+;   (#gsub! @language "/%*%s*([%w%p]+)%s*%*/" "%1")
+;   (#set! combined))
 
-((apply_expression
-   function: (apply_expression function: (_) @_func)
-   argument: (indented_string_expression (string_fragment) @injection.content))
- (#match? @_func "(^|\\.)writeShellScript(Bin)?$")
- (#set! injection.language "bash")
- (#set! injection.combined))
-
-(apply_expression
-  (apply_expression
-    function: (apply_expression
-      function: ((_) @_func)))
-    argument: (indented_string_expression (string_fragment) @injection.content)
-  (#match? @_func "(^|\\.)runCommand(((No)?(CC))?(Local)?)?$")
-  (#set! injection.language "bash")
-  (#set! injection.combined))
+; ; #-style Comments
+; ((comment) @language
+;   . ; this is to make sure only adjacent comments are accounted for the injections
+;   [
+;     (string_expression
+;       (string_fragment) @content)
+;     (indented_string_expression
+;       (string_fragment) @content)
+;   ]
+;   (#gsub! @language "#%s*([%w%p]+)%s*" "%1")
+;   (#set! combined))
 
 (apply_expression
-  function: ((_) @_func)
-  argument: (_ (_)* (_ (_)* (binding
-    attrpath: (attrpath (identifier) @_path)
-     expression: (indented_string_expression
-       (string_fragment) @injection.content))))
+  function: (_) @_func
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "regex")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "regex")))
+  ]
+  (#match? @_func "(^|\\.)match$")
+  (#set! combined))
+
+(binding
+  attrpath: (attrpath
+    (identifier) @_path)
+  expression: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+  ]
+  (#match? @_path "(^\\w+(Phase|Hook|Check)|(pre|post)[A-Z]\\w+|script)$"))
+
+(apply_expression
+  function: (_) @_func
+  argument: (_
+    (_)*
+    (_
+      (_)*
+      (binding
+        attrpath: (attrpath
+          (identifier) @_path)
+        expression: [
+          (string_expression
+            ((string_fragment) @content
+              (#set! language "bash")))
+          (indented_string_expression
+            ((string_fragment) @content
+              (#set! language "bash")))
+        ])))
   (#match? @_func "(^|\\.)writeShellApplication$")
   (#match? @_path "^text$")
-  (#set! injection.language "bash")
-  (#set! injection.combined))
+  (#set! combined))
+
+(apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+  ]
+  (#match? @_func "(^|\\.)runCommand((No)?CC)?(Local)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (_) @_func)
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "bash")))
+  ])
+  (#match? @_func "(^|\\.)write(Bash|Dash|ShellScript)(Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (_) @_func)
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "fish")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "fish")))
+  ])
+  (#match? @_func "(^|\\.)writeFish(Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "haskell")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "haskell")))
+  ])
+  (#match? @_func "(^|\\.)writeHaskell(Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "javascript")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "javascript")))
+  ])
+  (#match? @_func "(^|\\.)writeJS(Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "perl")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "perl")))
+  ])
+  (#match? @_func "(^|\\.)writePerl(Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "python")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "python")))
+  ])
+  (#match? @_func "(^|\\.)write(PyPy|Python)[23](Bin)?$")
+  (#set! combined))
+
+((apply_expression
+  function: (apply_expression
+    function: (apply_expression
+      function: (_) @_func))
+  argument: [
+    (string_expression
+      ((string_fragment) @content
+        (#set! language "rust")))
+    (indented_string_expression
+      ((string_fragment) @content
+        (#set! language "rust")))
+  ])
+  (#match? @_func "(^|\\.)writeRust(Bin)?$")
+  (#set! combined))
