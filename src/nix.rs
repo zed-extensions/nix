e812d4b1dfad9d29f84eda33de420fd0254cb37f
Mathias <hi@mathias.ninja>
2024-11-17 20:06:38 +0100

    Add support for nil language server (#13)
    Closes #9
    Supersedes #7 
    
    ## Configure Nixd
    
    Options:
    <https://github.com/nix-community/nixd/blob/main/nixd/docs/configuration.md>
    
    Example:
    [`settings.json`](https://zed.dev/docs/configuring-zed#settings-files)
    ```json
    {
    	"lsp": {
    		"nixd": {
    			"settings": {
    				"diagnostic": {
    					"suppress": [ "sema-extra-with" ]
    				}
    			}
    		}
    	}
    }
    ```
    
    ## Configure Nil
    
    Options:
    <https://github.com/oxalica/nil/blob/main/docs/configuration.md>
    
    Example:
    [`settings.json`](https://zed.dev/docs/configuring-zed#settings-files)
    ```json
    {
    	"lsp": {
    		"nil": {
    			"settings": {
     				"diagnostics": {
    					"ignored": [ "unused_binding" ]
    				}
    			}
    		}
    	}
    }
    ```
    
    ## Only use Nixd
    
    [`settings.json`](https://zed.dev/docs/configuring-zed#settings-files)
    ```json
    {
    	"languages": {
    		"Nix": {
    			"language_servers": [ "nixd", "!nil" ],
    		}
    	}
    }
    
    ```
    
    ## Only use Nil
    
    [`settings.json`](https://zed.dev/docs/configuring-zed#settings-files)
    ```json
    {
    	"languages": {
    		"Nix": {
    			"language_servers": [ "nil", "!nixd" ],
    		}
    	}
    }
    
    ```

--- src/nix.rs
+++ src/nix.rs
@@ -1,28 +1,53 @@
-use zed_extension_api::{self as zed, LanguageServerId, Result};
+mod language_servers;
 
-struct NixExtension;
+use zed_extension_api::{self as zed, serde_json, settings::LspSettings, LanguageServerId, Result};
+
+use crate::language_servers::{Nil, Nixd};
+
+struct NixExtension {
+    nil: Option<Nil>,
+    nixd: Option<Nixd>,
+}
 
 impl zed::Extension for NixExtension {
     fn new() -> Self {
-        Self
+        Self {
+            nil: None,
+            nixd: None,
+        }
     }
 
     fn language_server_command(
         &mut self,
-        _: &LanguageServerId,
+        language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let path = worktree.which("nixd").ok_or_else(|| {
-            "The Nix language server (nixd) is not available in your environment (PATH).
-                You can install it from https://github.com/nix-community/nixd."
-                .to_string()
-        })?;
+        match language_server_id.as_ref() {
+            Nil::LANGUAGE_SERVER_ID => {
+                let nil = self.nil.get_or_insert_with(Nil::new);
+                nil.language_server_command(language_server_id, worktree)
+            }
+            Nixd::LANGUAGE_SERVER_ID => {
+                let nixd = self.nixd.get_or_insert_with(Nixd::new);
+                nixd.language_server_command(language_server_id, worktree)
+            }
+            language_server_id => Err(format!("unknown language server: {language_server_id}")),
+        }
+    }
 
-        Ok(zed::Command {
-            command: path,
-            args: vec![],
-            env: vec![],
-        })
+    fn language_server_workspace_configuration(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<Option<zed::serde_json::Value>> {
+        let settings = LspSettings::for_worktree(language_server_id.as_ref(), worktree)
+            .ok()
+            .and_then(|lsp_settings| lsp_settings.settings.clone())
+            .unwrap_or_default();
+
+        let mut map = serde_json::Map::new();
+        map.insert(language_server_id.to_string(), settings);
+        Ok(Some(serde_json::json!(map)))
     }
 }
 
