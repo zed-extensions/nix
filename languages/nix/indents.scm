5f40c83ec5d71114e02e63fb71940637035e4cee
MrSubidubi <dev@bahn.sh>
2026-09-11 17:18:54 +0200

    Format Tree-sitter queries

--- languages/nix/indents.scm
+++ languages/nix/indents.scm
@@ -1,7 +1,20 @@
-(formals "}" @end) @indent
-(indented_string_expression "''" @end) @indent
-(parenthesized_expression ")" @end) @indent
-(attrset_expression "}" @end) @indent
-(let_attrset_expression "}" @end) @indent
-(rec_attrset_expression "}" @end) @indent
-(list_expression "]" @end) @indent
+(formals
+  "}" @end) @indent
+
+(indented_string_expression
+  "''" @end) @indent
+
+(parenthesized_expression
+  ")" @end) @indent
+
+(attrset_expression
+  "}" @end) @indent
+
+(let_attrset_expression
+  "}" @end) @indent
+
+(rec_attrset_expression
+  "}" @end) @indent
+
+(list_expression
+  "]" @end) @indent
